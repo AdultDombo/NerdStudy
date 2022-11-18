64361b9a2d7e1089e5278961ca07234f996b9e5d
junyongchoi <yjc0455@naver.com>
2022-11-18 18:24:02 +0900

    잘못 친부분 수정

--- 최소값 만들기/junyongchoi.cpp
+++ 최소값 만들기/junyongchoi.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include<vector>
+//#include <numeric>
 using namespace std;
 
 int solution(vector<int> A, vector<int> B)
@@ -10,6 +11,6 @@ int solution(vector<int> A, vector<int> B)
     sort(B.rbegin(), B.rend());
     for(int i; i < A.size(); i++) // A, B size가 같으므로
         answer += A[i] * B[i];
-    //inner_product(A.begin(), A.end(), B.begin(), 0) -> 스칼라 곱을 사용해도 됨
+    //answer = inner_product(A.begin(), A.end(), B.begin(), 0); //-> 스칼라 곱을 사용해도 됨
     return answer;
 }
